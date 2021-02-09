6a8a77587779b7a28291d19dfcb2ae2204005730
Snezhko-Vadim <59837673+Snezhko-Vadim@users.noreply.github.com>
2021-02-09 15:48:40 +0300

    commit #1

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App: React.FC = () => {
+  return(
+    <View>
+      <Text>
+        hello world!
+      </Text>
+    </View>
+  )
+}
+
+export default App;
