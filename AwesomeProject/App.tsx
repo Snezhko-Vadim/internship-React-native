cf7b04e9581a6ac4dedbfb7592204cb7e35b331b
Snezhko-Vadim <59837673+Snezhko-Vadim@users.noreply.github.com>
2021-02-11 16:40:03 +0300

    add launcher icon

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,26 +1,24 @@
-import React, {useEffect} from 'react';
-import {
-  View,
-} from 'react-native';
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
 
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen';
 import TxtComponent from './src/txt-component';
 
 const App: React.FC = () => {
-  useEffect(()=>{
-    if(SplashScreen!==null){
+  useEffect(() => {
+    if (SplashScreen !== null) {
       console.log(SplashScreen);
       setTimeout(() => {
         SplashScreen.hide();
         console.log(SplashScreen);
       }, 2000);
     }
-  },[]);
-  return(
+  }, []);
+  return (
     <View>
-      <TxtComponent text = 'Hello World!'/>
+      <TxtComponent text="Hello World!" />
     </View>
-  )
-}
+  );
+};
 
 export default App;
