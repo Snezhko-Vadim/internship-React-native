1007f06538b2d664d128f06fde3869a504f13c06
Snezhko-Vadim <59837673+Snezhko-Vadim@users.noreply.github.com>
2021-02-15 12:19:47 +0300

    add bottom tabs navigator

--- Football/App.tsx
+++ Football/App.tsx
@@ -1,24 +1,39 @@
+import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SplashScreen from 'react-native-splash-screen';
-import TxtComponent from 'src/txt-component';
+import HomeScreen from './src/screens/HomeScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   useEffect(() => {
     if (SplashScreen !== null) {
-      console.log(SplashScreen);
-      setTimeout(() => {
-        SplashScreen.hide();
-        console.log(SplashScreen);
-      }, 2000);
+      SplashScreen.hide();
     }
   }, []);
   return (
-    <View>
-      <TxtComponent text="Hello World!" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          labelStyle: styles.labelStyle,
+          tabStyle: styles.tabStyle,
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  labelStyle: { fontSize: 17 },
+  tabStyle: { justifyContent: 'center', alignItems: 'center' },
+});
 
 export default App;
