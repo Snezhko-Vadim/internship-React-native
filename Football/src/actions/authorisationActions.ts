725b9280dcef6a6b48503da5498a008de256186a
Snezhko-Vadim <59837673+Snezhko-Vadim@users.noreply.github.com>
2021-02-19 15:26:22 +0300

    update actions

--- Football/src/actions/authorisationActions.ts
+++ Football/src/actions/authorisationActions.ts
@@ -1,20 +1,24 @@
+const SIGN_IN = 'SIGN_IN';
+const RESTORE_TOKEN = 'RESTORE_TOKEN';
+const SIGN_OUT = 'SIGN_OUT';
+
 export const signIn = (token) => {
   return {
-    type: 'SIGN_IN',
+    type: SIGN_IN,
     token: token,
   };
 };
 
 export const signOut = (token) => {
   return {
-    type: 'SIGN_IN',
+    type: SIGN_OUT,
     token: token,
   };
 };
 
 export const restoreToken = (token) => {
   return {
-    type: 'RESTORE_TOKEN',
+    type: RESTORE_TOKEN,
     token: token,
   };
 };
